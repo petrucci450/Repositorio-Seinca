14265058d23cbd2943c6f712620f3f608b039d2e
Dany-creator <98239032+Dany-creator@users.noreply.github.com>
2026-04-17 14:41:07 -0400

    Update res_partner.py

--- bypass_vat_ve/models/res_partner.py
+++ bypass_vat_ve/models/res_partner.py
@@ -19,15 +19,16 @@ class ResPartner(models.Model):
             if country_code and country_code != 'VE' and not re.match(r'^[VEJ]-?\d{7,8}(-\d)?$', vat):
                 continue
 
-            if re.match(r'^[VEJ]-?\d{7}$', vat):
+            if re.match(r'^[VEJ]-?\d{7,8}$', vat):
                 continue
 
-            if re.match(r'^[VEJ]-?\d{8}-\d$', vat):
+            if re.match(r'^[VEJ]-?\d{8,9}-\d$', vat):
                 continue
 
             raise ValidationError(
                 'El número de identificación fiscal debe tener uno de estos formatos: '
                 'V0000000, V-0000000, E0000000, E-0000000, J0000000, J-0000000, '
+                'V00000000, V-00000000, E00000000, E-00000000, J00000000, J-00000000, '
                 'V00000000-0, V-00000000-0, E00000000-0, E-00000000-0, '
                 'J00000000-0 o J-00000000-0.'
             )
