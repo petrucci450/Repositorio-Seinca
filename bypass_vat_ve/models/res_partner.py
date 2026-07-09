eefbe161947c74c9eaeb3c250210c077e238620a
petrucci450 <113214090+petrucci450@users.noreply.github.com>
2026-07-09 15:20:00 -0400

    Prefijo C agregado

--- bypass_vat_ve/models/res_partner.py
+++ bypass_vat_ve/models/res_partner.py
@@ -16,19 +16,18 @@ class ResPartner(models.Model):
 
             country_code = partner.country_id.code if partner.country_id else False
 
-            if country_code and country_code != 'VE' and not re.match(r'^[VEJ]-?\d{7,8}(-\d)?$', vat):
+            if country_code and country_code != 'VE' and not re.match(r'^[VEJC]-?\d{7,8}(-\d)?$', vat):
                 continue
 
-            if re.match(r'^[VEJ]-?\d{7,8}$', vat):
+            if re.match(r'^[VEJC]-?\d{7,8}$', vat):
                 continue
 
-            if re.match(r'^[VEJ]-?\d{8,9}-\d$', vat):
+            if re.match(r'^[VEJC]-?\d{8,9}-\d$', vat):
                 continue
 
             raise ValidationError(
                 'El número de identificación fiscal debe tener uno de estos formatos: '
-                'V0000000, V-0000000, E0000000, E-0000000, J0000000, J-0000000, '
-                'V00000000, V-00000000, E00000000, E-00000000, J00000000, J-00000000, '
-                'V00000000-0, V-00000000-0, E00000000-0, E-00000000-0, '
-                'J00000000-0 o J-00000000-0.'
+                'V0000000, V-0000000, E0000000, E-0000000, J0000000, J-0000000, C0000000, C-0000000, '
+                'V00000000, V-00000000, E00000000, E-00000000, J00000000, J-00000000, C00000000, C-00000000, '
+                'V00000000-0, V-00000000-0, E00000000-0, E-00000000-0, J00000000-0, J-00000000-0, C00000000-0 o C-00000000-0.'
             )
